615ef760b8edd6c20a7a9ed92bdc6c53802772c4
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2019-09-26 07:15:33 +0530

    Updated UI

--- TimeSheets/settings.py
+++ TimeSheets/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'widget_tweaks',
+
+    # custom apps
     'core',
 ]
 
